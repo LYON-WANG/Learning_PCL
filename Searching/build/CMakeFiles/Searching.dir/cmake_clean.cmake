1c1c4f1a1a58303726f8a5d758c323412d747c7e
liangyu <liangyu@student.chalmers.se>
2021-02-21 00:35:15 +0100

    Add Searching

---
+++ Searching/build/CMakeFiles/Searching.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/Searching"
+  "../bin/Searching.pdb"
+  "CMakeFiles/Searching.dir/src/Searching.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Searching.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
